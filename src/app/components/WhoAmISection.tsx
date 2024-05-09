cb999e3fcd6b2ab952d7d1e3aa2504a6784d6570
Hajar Hamza <hajar.hamza1@gmail.com>
2024-05-09 18:24:06 +0200

    Experience & whoAmISection

---
+++ src/app/components/WhoAmISection.tsx
@@ -0,0 +1,14 @@
+export const WhoAmISection = () => (
+    <section className="rounded-xl w-full max-w-6xl mx-auto px-4 py-10 md:py-20 mb-10 bg-black dark:bg-white text-white dark:text-black">
+      <div className="max-w-6xl mx-auto h-24 text-black dark:text-white">
+        <h2
+          id="whoami"
+          className="text-5xl md:text-8xl font-bold text-left -mt-20 md:-mt-40"
+        >
+          Who am I?
+        </h2>
+      </div>
+      <p>This is the hero section</p>
+    </section>
+  );
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import { ExperienceSection } from "./components/ExperienceSection";
 import { HeroSection } from "./components/HeroSection";
+import { WhoAmISection } from "./components/WhoAmISection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between gap-32">
       <HeroSection />
-      {/* Who Am I */}
-      {/* Experience */}
+      <WhoAmISection />
+      <ExperienceSection />
       {/* Projects */}
       {/* Latest Code */}
       {/* Reach Out */}
