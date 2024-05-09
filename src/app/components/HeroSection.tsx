b1b970232926866c905ae473a18d37a0e60829aa
Hajar Hamza <hajar.hamza1@gmail.com>
2024-05-09 18:19:44 +0200

    add HeroSection with dummy copy

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,9 @@
+export const HeroSection = () => (
+    <section className="w-full max-w-6xl mx-auto px-4 py-10 md:py-20 mb-10">
+      <div className="max-w-6xl mx-auto h-24 text-black dark:text-white">
+        <h2 className="text-5xl md:text-8xl font-bold text-left">🧕🏼 🎒 💻</h2>
+      </div>
+      <p>This is the hero section</p>
+    </section>
+  );
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { HeroSection } from "./components/HeroSection";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between gap-32">
-      {/* Hero Section */}
+      <HeroSection />
       {/* Who Am I */}
       {/* Experience */}
       {/* Projects */}
