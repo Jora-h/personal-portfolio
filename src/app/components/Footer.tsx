01bfdbe2635b797c8a54b936f55c8d9cd69170d2
Hajar Hamza <hajar.hamza1@gmail.com>
2024-06-06 22:49:48 +0200

    latest changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,32 +1,17 @@
-'use client';
-
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-
 export const Footer = () => {
-    const [mounted, setMounted] = useState(false);
-    const { theme } = useTheme();
-
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-
-    if (!mounted) {
-        return null;
-    }
-
-    return (
-        <footer className="max-w-6xl mx-auto px-4 pt-10">
-            <hr className="bg-gray-200 border-0 dark:bg-gray-700 h-px"></hr>
-            <div className="flex md:flex-row justify-between items-end py-10">
-                <p className="font-light text-sm text-gray-500 dark:text-gray-400">
-                    <strong className="font-semibold">Hajar Hamza</strong> all rights
-                    reserved.
-                </p>
-                <p className="font-light text-sm text-gray-500 dark:text-gray-400">
-                    Built with {theme === "dark" ? "🤍" : "🖤"}
-                </p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="max-w-6xl mx-auto px-4 pt-10">
+      <hr className="bg-gray-200 border-0 dark:bg-gray-700 h-px"></hr>
+      <div className="flex md:flex-row justify-between items-end py-10">
+        <p className="font-light text-sm text-gray-500 dark:text-gray-400">
+          <strong className="font-semibold">Hajar Hamza</strong> all rights
+          reserved.
+        </p>
+        <p className="font-light text-sm text-gray-500 dark:text-gray-400">
+          Built with div <span className="inline-block dark:hidden">🖤</span>
+          <span className="hidden dark:inline-block">🤍</span>
+        </p>
+      </div>
+    </footer>
+  );
 };
